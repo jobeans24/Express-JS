cf5bba48c9a1f99ab2ece49d26c3875065172199
Josie Zobel <159351625+jobeans24@users.noreply.github.com>
2024-04-25 19:52:47 -0400

    working with post and how that connects with insomnia

--- 14-Stu_POST-Requests/Unsolved/server.js
+++ 14-Stu_POST-Requests/Unsolved/server.js
@@ -6,19 +6,29 @@ const app = express();
 
 // TODO: Create a GET method for `/api/reviews` that logs when a user's request has been received
 app.get('/api/reviews', (req, res) => {
-  // Your code here
+  // log that the request has been received
+  console.log(`${req.method} request received`);
 });
 
 // TODO: Create a POST request for `/api/reviews` that logs when a user's request has been received
-// Your code here
+// log that the request has been received
+app.post('/api/reviews', (req, res) => {
+  console.log(`${req.method} request received`);
+});
 
 // TODO: Create a GET request for `api/upvotes` that logs when a user's request has been received
 app.get('/api/upvotes', (req, res) => {
-  // Your code here
+  // logs that the request has been received
+  console.log(`${req.method} request received`);
 });
 
 // TODO: Create a POST request for `api/upvotes` that logs when a user's request has been received
 // Your code here
+app.post('/api/upvotes', (req, res) => {
+  console.log(`${req.method} request received`);
+});
+
+
 
 app.listen(PORT, () =>
   console.log(`Express server listening on port ${PORT}!`)
