b09c36470be336085111221b2577fc080039fef2
Josie Zobel <159351625+jobeans24@users.noreply.github.com>
2024-04-25 19:23:33 -0400

    working with express start and insomnia

--- 12-Stu_Insomnia/Unsolved/server.js
+++ 12-Stu_Insomnia/Unsolved/server.js
@@ -1,7 +1,10 @@
-const express = require('express');
+let express = require('express');
 // TODO: Require the json file located in `/db`
+const data = require('./db/repos.json');
 
 // TODO: Create an `app` variable set to the value of `express()`
+const app = express();
+
 
 app.get('/', (req, res) => {
   res.send(
@@ -10,5 +13,12 @@ app.get('/', (req, res) => {
 });
 
 // TODO: Create a GET route for `/api` that will return the content of our json file
+app.get('/api', (req, res) => {
+  res.json(data);
+});
+
 
 // TODO: Have the app listen on port 3001
+app.listen(3001, () => {
+  console.log('Server is running on http://localhost:3001');
+});
